b454a436af9c3fa6bcbf071114cab906dbcf6ebd
khatharsis42 <hguelque@gmail.com>
2021-02-11 17:32:06 +0100

    Made Everything Right: Premier test de multijoueur

--- core/src/com/mygdx/game/BigRedButton.kt
+++ core/src/com/mygdx/game/BigRedButton.kt
@@ -2,23 +2,28 @@ package com.mygdx.game
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
+import com.badlogic.gdx.Net
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.net.SocketHints
+import java.io.IOException
 import java.lang.System.load
 
 
 
-class BigRedButton(x:Float, y:Float, width:Float, height:Float) {
+class BigRedButton(idleTexture: Texture, pushedTexture: Texture, x:Float, y:Float, width:Float, height:Float) {
     var x:Float
     var y:Float
     var width:Float
     var height:Float
     var counter:Int = 0
 
-    val idle :  Sprite = Sprite(Texture("redButtonIdle.png"))
-    val pushed: Sprite = Sprite(Texture("redButtonPushed.png"))
+    val idle :  Sprite = Sprite(idleTexture)
+    val pushed: Sprite = Sprite(pushedTexture)
     var active:Sprite = idle
+
+
 
     init {
         this.x = x
@@ -39,14 +44,7 @@ class BigRedButton(x:Float, y:Float, width:Float, height:Float) {
         this.x = x
         this.y = y
     }
-    fun translateX(x:Int) {
-        this.x += x
-        updateSprite()
-    }
-    fun translateY(y:Int) {
-        this.y += y
-        updateSprite()
-    }
+
     fun stringPosition (): String {
         return "x = $x; y = $y"
     }
@@ -56,13 +54,20 @@ class BigRedButton(x:Float, y:Float, width:Float, height:Float) {
         this.height = height
     }
     fun update(batch: SpriteBatch) {
-
         active.draw(batch)
     }
-    fun isClicked(iX:Float, iY:Float) {
+    fun isClicked(iX:Float, iY:Float, id:String, ip:String) {
         if (x <= iX && iX <= x + width) {
                 if (y <= iY && iY <= y + height) {
-                    println("Button Pressed !")
+                    val textToSend = "Button Pressed by $id! \n"
+                    val socketHints = SocketHints()
+                    socketHints.connectTimeout = 4000
+                    val socket = Gdx.net.newClientSocket(Net.Protocol.TCP,ip,9021,socketHints)
+                    try {
+                        socket.getOutputStream().write(textToSend.toByteArray())
+                    } catch(e: IOException) {
+                        e.printStackTrace()
+                    }
                     active = pushed
                     counter++
                 }
--- core/src/com/mygdx/game/PushTheButton.kt
+++ core/src/com/mygdx/game/PushTheButton.kt
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 
 class PushTheButton : ApplicationAdapter() {
@@ -13,14 +14,20 @@ class PushTheButton : ApplicationAdapter() {
     lateinit var coordButton: BitmapFont
     lateinit var coordMouse: BitmapFont
 
+    var id = "PC de Hadrien"
+    var ip = "157.159.193.94" //L'ip de mon PC fixe
+
     override fun create() {
         batch = SpriteBatch()
-        button = BigRedButton(250f, 0f, 200f, 450f)
+        button = BigRedButton(
+                Texture("redButtonIdle.png"),
+                Texture("redButtonPushed.png"),
+                250f, 0f, 200f, 450f)
         coordButton = BitmapFont()
         coordMouse = BitmapFont()
         coordButton.setColor(0f,0f,0f,1f)
         coordMouse.setColor(0f,0f,0f,1f)
-
+        ThreadMaker().getThread().start()
     }
 
     override fun render() {
@@ -35,18 +42,8 @@ class PushTheButton : ApplicationAdapter() {
         if (button.isClickable())
             if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
                 //println("Souris")
-                button.isClicked(Gdx.input.getX().toFloat(), Gdx.input.getY().toFloat())
+                button.isClicked(Gdx.input.getX().toFloat(), Gdx.input.getY().toFloat(), id, ip)
             }
-
-        // Useless but worth keeping
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
-            button.translateX( -1 )
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
-            button.translateX( 1 )
-        if (Gdx.input.isKeyPressed(Input.Keys.UP))
-            button.translateY( 1 )
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN))
-            button.translateY( -1 )
 
         button.update(batch)
         batch.end()
