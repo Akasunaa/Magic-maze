905cce89aba5ca5f45e60d2e986da5abfa265256
khatharsis42 <hguelque@gmail.com>
2021-02-22 01:01:28 +0100

    Rajout d'une configuration dans le launcher, afin de mettre la résolution à 720x1280 ie en 16:9; solution temporaire, demander à Yrax pour fix

--- desktop/src/com/mygdx/game/MMmain.java
+++ desktop/src/com/mygdx/game/MMmain.java
@@ -1,10 +1,22 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class MMmain {
     public static void main(String[] arg) {
+
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.height = 720;
+        config.width = 1280;
+        config.forceExit = false;
+        // On créé une configuration pour l'application, et on la fait tourner en 720x1280
+        // Solution temporaire, il faudrait demander à Yrax comment il a fait pour avoir
+        // un truc qui tourne en 1920x1080 de manière modulaire
+
         MMgame MM = new MMgame();
-        LwjglApplication launcher = new LwjglApplication(MM);
+        // C'est techniquement inutile de créer cette variable MM
+        // Rappel sur les conventions: les variables commencent toujours par une minuscule >:(
+        LwjglApplication launcher = new LwjglApplication(MM, config);
     }
 }
