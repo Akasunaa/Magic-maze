718915a1c47f436fd4a19e23842e1538653c5267
khatharsis42 <hguelque@gmail.com>
2021-03-15 18:52:15 +0100

    Commit pour pouvoir build en un .jar

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,8 +1,10 @@
+package com.mygdx.game.desktop;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.menu.MagicGame;
 
-public class GameLauncher {
+public class DesktopLauncher {
     public static void main (String[] args) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.height = 720;
