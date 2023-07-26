cdbdc3bc8bbcb30618fde8788f905e5028ab20c1
Catherine Guelque <hadrien.guelque@telecom-sudparis.eu>
2023-07-26 17:54:38 +0200

    Update to gradle
    Signed-off-by: Catherine Guelque <hadrien.guelque@telecom-sudparis.eu>

---
+++ core/src/com/utils/Enums.kt
@@ -0,0 +1,11 @@
+package com.utils
+
+enum class Color {
+    GREEN, PURPLE, YELLOW, ORANGE, NONE;
+}
+
+enum class Directions {
+    SOUTH, EAST, NORTH, WEST;
+
+    operator fun plus(other: Int): Int = Directions.values().indexOf(this) + other
+}
